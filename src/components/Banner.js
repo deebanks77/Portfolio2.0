0019c73a0cca4a22fdfc1313426ebb93d15d9622
Banky <mac@macs-MacBook-Pro.local>
2023-04-29 13:13:54 +0100

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -53,13 +53,9 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__fadeIn" : ""
-                  }
-                >
+                <div className={isVisible ? "animate__animated" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
-                  <h1>
+                  <h1 className="intro">
                     {`Hi! I'm Bankole`}{" "}
                     <span className="txt-rotate">
                       <span className="wrap">{text}</span>
@@ -91,11 +87,7 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
               {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__zoomIn" : ""
-                  }
-                >
+                <div className={isVisible ? "animate__animated" : ""}>
                   <img src={headerImg} alt="Header Img" />
                 </div>
               )}
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -85,11 +85,7 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__fadeIn" : ""
-                  }
-                >
+                <div className={isVisible ? "animate__animated" : ""}>
                   <h2>Get In Touch</h2>
                   <form onSubmit={handleSubmit}>
                     <Row>
